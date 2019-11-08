7cf45253c532d19f66a74d5a7fe4159949057532
Charles <brown.charlieangel@gmail.com>
2019-11-08 09:40:53 +0200

    Add files via upload

---
+++ DubStep/DubStep/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DubStep
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      GetSongTitle();
+      PrintOriginalSongName();
+    }
+
+    private static void PrintOriginalSongName()
+    {
+      Console.WriteLine(RemoveWubsFromTitle(GetSongTitle()));
+    }
+
+    private static string GetSongTitle()
+    {
+      Console.WriteLine("Hello.\nPlease Enter a DubStep Song title: ");
+      string title = Console.ReadLine();
+      EmptyStringTest(title);
+      return title;
+    }
+
+    private static string EmptyStringTest(string title)
+    {
+      while (title == "")
+        title = Console.ReadLine();
+      return title;
+    }
+
+    public static string RemoveWubsFromTitle(string songTitle)
+    {
+      string[] splitSongTitle = songTitle.Split("WUB");
+      string title = SongNameBuilder(splitSongTitle);
+
+      songTitle = title.Trim();
+
+      return songTitle;
+    }
+
+    private static string SongNameBuilder(string[] splitSongTitle)
+    {
+      string title = "";
+      return ConcatSplitString(splitSongTitle, title);
+    }
+
+    private static string ConcatSplitString(string[] splitSongTitle, string title)
+    {
+      foreach (var word in splitSongTitle)
+      {
+        title = SongTitleBuilder(title, word);
+      }
+
+      return title;
+    }
+
+    private static string SongTitleBuilder(string title, string word)
+    {
+      if (word != "")
+        title += (word + " ");
+      return title;
+    }
+  }
+}
---
+++ PrintTheMatrix/PrintTheMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrintTheMatrix
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] intArr = {1, 2, 3, 4, 5};
+
+      for (int i = intArr.Length-1; i >= 0; i--)
+      {
+        Console.WriteLine("");
+        for (int j = 1; j <= intArr.Length; j++)
+        {
+          if (j <= intArr[i])
+          {
+            Console.Write(i+1);
+          } else 
+            Console.Write(j);
+        }
+        
+      }
+    }                                                                                                                                                                                                   
+
+  }
+}
